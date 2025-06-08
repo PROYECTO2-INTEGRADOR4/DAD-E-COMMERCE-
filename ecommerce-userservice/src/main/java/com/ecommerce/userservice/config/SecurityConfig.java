7e207c25e54dfc185d7dcbcd1ebf7acab8058431
Tu Nombre <noelia.rivera@upeu.edu.pe>
2025-06-08 17:41:51 -0500

    auth

--- ecommerce-userservice/src/main/java/com/ecommerce/userservice/config/SecurityConfig.java
+++ ecommerce-userservice/src/main/java/com/ecommerce/userservice/config/SecurityConfig.java
@@ -1,8 +1,9 @@
 package com.ecommerce.userservice.config;
 
-import jakarta.ws.rs.HttpMethod;
+
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -11,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
