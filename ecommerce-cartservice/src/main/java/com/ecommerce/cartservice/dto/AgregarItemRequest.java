587d9a4a6e5d27c12a37f1c620cafa26f3d49696
daryn <darely.quispes@upeu.edu.pe>
2025-06-09 11:07:16 -0500

    carritov1

---
+++ ecommerce-cartservice/src/main/java/com/ecommerce/cartservice/dto/AgregarItemRequest.java
@@ -0,0 +1,14 @@
+package com.ecommerce.cartservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AgregarItemRequest {
+    private Long carritoId;
+    private Long productoVarianteId;
+    private Integer cantidad;
+}
---
+++ ecommerce-cartservice/src/main/java/com/ecommerce/cartservice/dto/ProductoVarianteDto.java
@@ -0,0 +1,18 @@
+package com.ecommerce.cartservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductoVarianteDto {
+    private Long id;
+    private String nombreproducto;
+    private Map<String, String> atributos;
+    private BigDecimal precio;
+}
